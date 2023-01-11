cec4995f03d628e0421bd8f6899e5b609cfc430a
“Sarah <sarahcatherine311@gmail.com>
2023-01-11 12:37:13 -0700

    Fishish part 1 of comparisons file

--- comparisons.js
+++ comparisons.js
@@ -34,15 +34,19 @@ console.log("Is numberTeachers not equal to numberStudents?", numberTeachers !==
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
 // this should log: true
+console.log("Is numberStudents greater than or equal to 20?", numberStudents >= 20);
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
+console.log("Is numberStudents greater than or equal to 21?", numberStudents >= 21);
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
+console.log("Is numberStudents less than or equal to 20?", numberStudents <= 20);
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
 // this should log: true
+console.log("Is numberStudents less than or equal to 21?", numberStudents <= 21);
 
 
 // #-------------------
